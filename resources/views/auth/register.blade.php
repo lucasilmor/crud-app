5d84a4094798ec19fa3ef08f9a37ccfd997d2d82
lucasilmor <lucassilmor0@hotmail.com>
2024-04-28 01:17:19 -0300

    mudei a tela de welcome para parecer virus

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,6 @@
 <x-guest-layout>
+
+
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -49,4 +51,6 @@
             </x-primary-button>
         </div>
     </form>
+
+    
 </x-guest-layout>
